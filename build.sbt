7d67918ec99fb62121019ca80dfb470d7a6fa68c
David Flores <david.flores@elastacloud.com>
2020-06-10 23:49:13 +0200

    initial commit

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "whitechapelai"
+
+version := "0.1"
+
+scalaVersion := "2.12.11"
+
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.1"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test"
---
+++ src/main/scala/whitechapelai.scala
@@ -0,0 +1,13 @@
+/**
+ * Example of main class
+ */
+object whitechapelai extends App {
+  println("Welcome to whitechapelai !!")
+
+  /**
+   * Increments a number by one
+   * @param a number to be incremented
+   * @return number incremented
+   */
+  def sum(a:Int) : Int = a + 1
+}
